d92d0c21f26e91c948d08122a6f9c0daa2b9dd9c
Joe Kutner <jpkutner@gmail.com>
2017-05-03 16:51:59 -0500

    Source script instead of piping to bash

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -2,5 +2,5 @@
 
 if [ -n "$HEROKU_EXEC_URL" ]; then
   export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/heroku-exec\./heroku-exec-staging\./)"
-  [ -z "$SSH_CLIENT" ] && curl -sSL $HEROKU_EXEC_URL | bash
+  [ -z "$SSH_CLIENT" ] && source <(curl -sSL $HEROKU_EXEC_URL)
 fi
