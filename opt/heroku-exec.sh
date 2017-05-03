9b91f27ed443aafb978fb64ba6a0d6c09d4c1395
Joe Kutner <jpkutner@gmail.com>
2017-05-03 17:02:57 -0500

    Remove staging tweak from dup

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 if [ -n "$HEROKU_EXEC_URL" ]; then
-  export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/heroku-exec\./heroku-exec-staging\./)"
   [ -z "$SSH_CLIENT" ] && source <(curl --retry 3 -sSL $HEROKU_EXEC_URL)
 fi
