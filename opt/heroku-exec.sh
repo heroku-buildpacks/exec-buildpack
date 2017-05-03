0a2968aa9e53a0104c693ce5fb0cd503cbd6f9ff
Joe Kutner <jpkutner@gmail.com>
2017-05-03 17:01:03 -0500

    Created dup profile script

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -2,5 +2,5 @@
 
 if [ -n "$HEROKU_EXEC_URL" ]; then
   export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/heroku-exec\./heroku-exec-staging\./)"
-  [ -z "$SSH_CLIENT" ] && source <(curl -sSL $HEROKU_EXEC_URL)
+  [ -z "$SSH_CLIENT" ] && source <(curl --retry 3 -sSL $HEROKU_EXEC_URL)
 fi
