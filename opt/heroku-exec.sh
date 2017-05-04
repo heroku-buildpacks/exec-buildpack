c5857946092df439ca56967acea0fd586dd84f0c
Joe Kutner <jpkutner@gmail.com>
2017-05-04 08:46:45 -0500

    Better sed exp

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 if [ -n "$HEROKU_EXEC_URL" ]; then
+  export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/heroku-exec\.herokuapp/heroku-exec-staging\.herokuapp/)"
   [ -z "$SSH_CLIENT" ] && source <(curl --retry 3 -sSL $HEROKU_EXEC_URL)
 fi
