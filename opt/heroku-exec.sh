89cc49bda2f52c76a85af276bb06fdd2f6e8564e
Joe Kutner <jpkutner@gmail.com>
2017-05-18 16:07:20 -0500

    Refactoring

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -n "$HEROKU_EXEC_URL" ]; then
+if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
   export HEROKU_EXEC_URL="$(echo "$HEROKU_EXEC_URL" | sed s/heroku-exec\.herokuapp/heroku-exec-staging\.herokuapp/)"
-  [ -z "$SSH_CLIENT" ] && source <(curl -sSL $HEROKU_EXEC_URL)
+  source <(curl --fail -sSL "$HEROKU_EXEC_URL")
 fi
