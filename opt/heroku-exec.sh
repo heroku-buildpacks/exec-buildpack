29acdd10da4f43a2f4a79a4879322c509dcd8e3b
BuildpackTester <jpkutner@gmail.com>
2017-11-21 12:43:50 -0600

    Add --max-time to curl request

--- opt/heroku-exec.sh
+++ opt/heroku-exec.sh
@@ -8,5 +8,5 @@ if [ -n "$HEROKU_EXEC_URL" ] && [ -z "$SSH_CLIENT" ]; then
   ssh_pid="$(ps -C ssh -o pid=)"
   [ -n "$sshd_pid" ] && kill -9 "$sshd_pid"
   [ -n "$ssh_pid" ] && kill -9 "$ssh_pid"
-  source <(curl --fail -sSL "$HEROKU_EXEC_URL")
+  source <(curl --fail --max-time 5 -sSL "$HEROKU_EXEC_URL")
 fi
